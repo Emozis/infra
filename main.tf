cbe2e2a407f80a22d6dcbd3dedbd75890216e935
MinkyoDev <minkyo.dev@gmail.com>
2024-12-19 15:07:49 +0000

    [Refactor]: main.tf 수정

--- main.tf
+++ main.tf
@@ -25,6 +25,21 @@ module "cloudfront" {
   cloudfront_access_identity_path = module.s3.cloudfront_access_identity_path
 }
 
+module "emogi_app" {
+  source          = "./modules/ec2"
+  instance_name   = "emogi-ec2-app"
+  ami_id          = "ami-0f1e61a80c7ab943e"
+  instance_type   = "t3.micro"
+  user_data_path  = "script/initail_server_setting.sh"
+  vpc_id          = module.vpc.vpc_id
+  subnet_id       = module.vpc.public_subnet_1_id
+  bucket_name = local.env_vars["BUCKET_NAME"]
+  key_name = "emogi-keypair"
+
+  target_group_arn = module.alb.target_group_arn
+  alb_security_group_id = module.alb.security_group_id
+}
+
 module "rds" {
   source = "./modules/rds"
 
@@ -39,16 +54,7 @@ module "rds" {
     module.vpc.private_subnet_2_id
   ]
   
-  allowed_security_groups = [module.emogi_app.security_group_id]  # EC2의 보안그룹 ID
-}
-
-module "env_secret" {
-  source = "./modules/secrets-manager"
-  
-  secret_name  = "/emogi/env-variables"
-  description  = "Environment variables for Emogi application"
-  environment  = "prod"
-  secret_value = file("${path.module}/.env.prod")
+  allowed_security_groups = [module.emogi_app.security_group_id]
 }
 
 module "ssm_parameters" {
@@ -67,32 +73,11 @@ module "ssm_parameters" {
       value       = module.cloudfront.cloudfront_domain_name
       environment = "prod"
     }
-    "/emogi/secrets/env_secret_name" = {
-      description = "Emogi Environment Variables Secret Name"
-      type        = "String"
-      value       = module.env_secret.secret_id
-      environment = "prod"
-    }
-    "/emogi/env-variables" = {
+    "/emogi/ec2/env-variables" = {
       description = "Emogi Environment Variables"
       type        = "SecureString"
       value       = file("${path.module}/.env.prod")
       environment = "prod"
     }
   }
-}
-
-module "emogi_app" {
-  source          = "./modules/ec2"
-  instance_name   = "emogi-ec2-app"
-  ami_id          = "ami-0f1e61a80c7ab943e"
-  instance_type   = "t3.micro"
-  user_data_path  = "script/initail_server_setting.sh"
-  vpc_id          = module.vpc.vpc_id
-  subnet_id       = module.vpc.public_subnet_1_id
-  bucket_name = local.env_vars["BUCKET_NAME"]
-  key_name = "emogi-keypair"
-
-  target_group_arn = module.alb.target_group_arn
-  alb_security_group_id = module.alb.security_group_id
 }
